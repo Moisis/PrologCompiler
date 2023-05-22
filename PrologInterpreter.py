56dfaefa563461302687ce3aa413cfcb1398b9f5
Moisis <moisis.george@yahoo.com>
2023-05-22 22:26:41 +0300

    Fix some DFAS , added slight animation

--- PrologInterpreter.py
+++ PrologInterpreter.py
@@ -28,8 +28,7 @@ class Editor(object):
         self.file_path = None
         self.root.title("Prolog Interpreter")
         self.lol = 0
-        self.lol = 0
-        self.lol2 = 0
+        self.arthcounter = 0
 
         # Create a rule label
 
@@ -75,18 +74,6 @@ class Editor(object):
 
         self.query_editor.config(wrap="word", undo=True)
 
-        # Testinput
-
-        self.diagraminputlabel = Label(root, text="Test input ", padx=10, pady=1)
-
-        self.diagraminputlabel.grid(sticky=W, row=2, column=2, columnspan=2, pady=3)
-
-        self.diagrambox = Text(root, width=40, height=2, padx=10, pady=10)
-
-        self.diagrambox.grid(sticky=W, row=3, column=2, pady=3, padx=10)
-
-        self.diagrambox.config(wrap="word", undo=True)
-
         # Create a run button which runs the query against our rules and outputs the
         # results in our solutions text box / editor.
 
@@ -97,13 +84,13 @@ class Editor(object):
             width=20,
             command=self.run_query,
         )
-        self.drawdfabutton = Button(
-            root,
-            text="Start Animation",
-            height=2,
-            width=30,
-            command=self.drawStepbystep,
-        )
+        # self.drawdfabutton = Button(
+        #     root,
+        #     text="Start Animation",
+        #     height=2,
+        #     width=30,
+        #     command=self.drawStepbystep,
+        # )
         # self.drawparsetree = Button(
         #     root,
         #     text="Draw Parse tree",
@@ -111,18 +98,18 @@ class Editor(object):
         #     width=20,
         #     command=self.draw_ParseTree,
         # )
-        self.resetanima = Button(
-            root,
-            text="Reset Diagram",
-            height=2,
-            width=20,
-            command=self.resetanimation,
-        )
+        # self.resetanima = Button(
+        #     root,
+        #     text="Reset Diagram",
+        #     height=2,
+        #     width=20,
+        #     command=self.resetanimation,
+        # )
 
         self.run_button.grid(sticky=E, row=3, column=1, pady=3, padx=10)
-        self.drawdfabutton.grid(sticky=E, row=3, column=2, pady=3, padx=10)
+        # self.drawdfabutton.grid(sticky=E, row=3, column=2, pady=3, padx=10)
         # self.drawparsetree.grid(sticky=E, row=4, column=2, pady=3, padx=10)
-        self.resetanima.grid(sticky=E, row=3, column=3, pady=3, padx=10)
+        # self.resetanima.grid(sticky=E, row=3, column=3, pady=3, padx=10)
 
         # Create a solutions label
 
@@ -150,6 +137,27 @@ class Editor(object):
         self.label1 = Label(root, image=photo)
         self.label1.image = photo
         self.label1.grid(row=1, column=2)
+
+        # LABEL LEGEND
+
+        self.diagraminputlabel = Label(root, text="MAP LEGEND ", padx=10, pady=1)
+
+        self.diagraminputlabel.grid(sticky=W, row=2, column=2, columnspan=2, pady=3)
+
+        self.diagrambox = Text(root, width=50, height=4, padx=10, pady=10)
+
+        self.diagrambox.grid(sticky=W, row=3, column=2, pady=3, padx=10)
+
+        self.diagrambox.config(state=DISABLED, undo=True)
+
+        # Testinput
+
+        self.diagramLEGEND = Label(root, text="Test input", padx=10, pady=1)
+
+        self.diagramLEGEND.grid(sticky=W, row=2, column=3, columnspan=2, pady=3)
+        self.legend = Text(root, width=50, height=4, padx=10, pady=10)
+
+        self.legend.grid(row=3, column=3, columnspan=2, padx=10, pady=7)
 
         menu_bar = Menu(root)
 
@@ -220,12 +228,17 @@ class Editor(object):
         )
         functionmenu.add_separator()
         functionmenu.add_command(
-            label="dfa_arithmetic_create", underline=1, command=self.dfa_arithmetic_create
+            label="dfa_arithmetic_create", underline=1, command=self.dfa_arithmetic_createfinal
         )
+        functionmenu.add_command(
+            label="animatearthmetic", underline=1, command=self.animatearthmetic
+        )
+        functionmenu.add_separator()
 
         functionmenu.add_command(
             label="dfa_relational_create", underline=1, command=self.dfa_relational_create
         )
+        functionmenu.add_separator()
         functionmenu.add_command(
             label="dfa_integer_create", underline=1, command=self.dfa_integer_create
         )
@@ -297,7 +310,6 @@ class Editor(object):
         if self.lol > (len(query_text)):
             self.lol = 0
 
-
         else:
             query1 = ""
             for i in range(self.lol):
@@ -325,20 +337,75 @@ class Editor(object):
         )
         dfa.show_diagram(input_str=query_text, filename='Digraph', format_type="png", path="test-graphs", view=False)
 
-    def dfa_arithmetic_create(self):
+    def animatearthmetic(self):
+        alphabetcaps = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                        'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        alphabetsmall = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                         't', 'u', 'v', 'w', 'x', 'y', 'z']
+        nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        arthemticoperators = ['*', '/', '+', '-']
+
+        self.diagrambox.config(state=NORMAL)
+        self.diagrambox.delete("1.0", END)
+        self.diagrambox.insert(END, "Operaters(P) : * | / | + | - \n")
+        self.diagrambox.insert(END, "Others(L) : [0-9] | [A-Z] | [a-z] | . | ; | : | _ | % | ( | ) | < | > | =")
+        self.diagrambox.config(state=DISABLED)
+        query = self.legend.get(1.0, "end-1c")
+        self.arthcounter += 1
+        if self.arthcounter > (len(query)):
+            self.arthcounter = 0
+        else:
+            quert = ""
+            for i in range(self.arthcounter):
+                if (query[i] in arthemticoperators):
+                    quert = quert + 'P'
+                    print(quert)
+                    self.dfa_arithmetic_create(quert)
+                    self.draw_DFA()
+                else:
+                    quert = quert + 'L'
+                    print(quert)
+                    self.dfa_arithmetic_create(quert)
+                    self.draw_DFA()
+
+            time.sleep(0.8)
+            root.update()
+            self.animatearthmetic()
+
+
+    def dfa_arithmetic_createfinal(self):
+        self.diagrambox.config(state=NORMAL)
+        self.diagrambox.delete("1.0", END)
+        self.diagrambox.insert(END, "Operaters(P) : * | / | + | - \n")
+        self.diagrambox.insert(END, "Others(L) : [0-9] | [A-Z] | [a-z] | . | ; | : | _ | % | ( | ) | < | > | =")
+        self.diagrambox.config(state=DISABLED)
+        query2 = self.legend.get(1.0, "end-1c")
+        arthemticoperators = ['*', '/', '+', '-']
+        query3 = ''
+        for i in range(len(query2)):
+            if (query2[i] in arthemticoperators):
+                query3 = query3+ 'P'
+            else:
+                query3 = query3 + 'L'
+
+        self.dfa_arithmetic_create(query3)
+        self.draw_DFA()
+
+
+
+    def dfa_arithmetic_create(self, querytext):
         dfa = VisualDFA(
             # [A-Za-z . * - + / ; : _ % ( ) < > = ]   <= >= :- <>
             states={"q0", "q1", "qD"},
             # input_symbols={"* / + -", "[A-Z a-z 0-9 .  ; : _ % ( ) < > = ]"},
-            input_symbols={"* | / | + | -", "[A-Z] | [a-z] | . | ; | : | _ | % | ( | ) | < | > | ="},
+            input_symbols={"P", "L"},
             transitions={
                 # "q0": {"* / + -": "q1", "[A-Z a-z 0-9 .  ; : _ % ( ) < > = ]": "qD"},
                 # "q1": {"* / + -": "qD", "[A-Z a-z 0-9 .  ; : _ % ( ) < > = ]": "qD"},
-                # # "qD": {"* / + -": "qD", "[A-Z a-z 0-9 .  ; : _ % ( ) < > = ]": "qD"},
-
-                "q0": {"* | / | + | -": "q1", "[A-Z] | [a-z] | . | ; | : | _ | % | ( | ) | < | > | =": "qD"},
-                "q1": {"* | / | + | -": "qD", "[A-Z] | [a-z] | . | ; | : | _ | % | ( | ) | < | > | =": "qD"},
-                "qD": {"* | / | + | -": "qD", "[A-Z] | [a-z] | . | ; | : | _ | % | ( | ) | < | > | =": "qD"},
+                #  "qD": {"* / + -": "qD", "[A-Z a-z 0-9 .  ; : _ % ( ) < > = ]": "qD"},
+                "q0": {"P": "q1", "L": "qD"},
+                "q1": {"P": "qD", "L": "qD"},
+                "qD": {"P": "qD", "L": "qD"},
                 # "q0": {"a": "q1", "b": "qD"},
                 # "q1": {"a": "qD", "b": "qD"},
                 # "qD": {"a": "qD", "b": "qD"},
@@ -346,9 +413,7 @@ class Editor(object):
             initial_state="q0",
             final_states={"q1"},
         )
-        dfa.show_diagram(input_str="", filename='Digraph', format_type="png", path="test-graphs", view=False)
-
-        self.draw_DFA()
+        dfa.show_diagram(input_str=querytext,filename='Digraph', format_type="png", path="test-graphs", view=False)
 
     def dfa_relational_create(self):
         dfa = VisualDFA(
@@ -400,11 +465,16 @@ class Editor(object):
             states={"q0", "q1", "q2", "q3", "qD"},
             input_symbols={"[0-9]", ".", "[A-Z] | [a-z] | * |  - |  + |  / |  ; |  : |  _ |  % | ( | ) | < | > | ="},
             transitions={
-                "q0": {"[0-9]": "q1", ".": "qD", "[A-Z] | [a-z] | * |  - |  + |  / |  ; |  : |  _ |  % | ( | ) | < | > | =": "qD"},
-                "q1": {"[0-9]": "q1", ".": "q2", "[A-Z] | [a-z] | * |  - |  + |  / |  ; |  : |  _ |  % | ( | ) | < | > | =": "qD"},
-                "q2": {"[0-9]": "q3", ".": "qD", "[A-Z] | [a-z] | * |  - |  + |  / |  ; |  : |  _ |  % | ( | ) | < | > | =": "qD"},
-                "q3": {"[0-9]": "q3", ".": "qD", "[A-Z] | [a-z] | * |  - |  + |  / |  ; |  : |  _ |  % | ( | ) | < | > | =": "qD"},
-                "qD": {"[0-9]": "qD", ".": "qD", "[A-Z] | [a-z] | * |  - |  + |  / |  ; |  : |  _ |  % | ( | ) | < | > | =": "qD"},
+                "q0": {"[0-9]": "q1", ".": "qD",
+                       "[A-Z] | [a-z] | * |  - |  + |  / |  ; |  : |  _ |  % | ( | ) | < | > | =": "qD"},
+                "q1": {"[0-9]": "q1", ".": "q2",
+                       "[A-Z] | [a-z] | * |  - |  + |  / |  ; |  : |  _ |  % | ( | ) | < | > | =": "qD"},
+                "q2": {"[0-9]": "q3", ".": "qD",
+                       "[A-Z] | [a-z] | * |  - |  + |  / |  ; |  : |  _ |  % | ( | ) | < | > | =": "qD"},
+                "q3": {"[0-9]": "q3", ".": "qD",
+                       "[A-Z] | [a-z] | * |  - |  + |  / |  ; |  : |  _ |  % | ( | ) | < | > | =": "qD"},
+                "qD": {"[0-9]": "qD", ".": "qD",
+                       "[A-Z] | [a-z] | * |  - |  + |  / |  ; |  : |  _ |  % | ( | ) | < | > | =": "qD"},
 
             },
             initial_state="q0",
